08274c93bb4829aa0145c72fda5d30fb079b6e91
Takuya Takahashi <info@yujilow.net>
2017-01-05 19:25:55 +0900

    stored to state: not storage

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const {Menu, app, BrowserWindow} = require('electron');
 var tabs;
 module.exports = {
-  init: function(conf_path){
+  load: function(conf_path){
     tabs = JSON.parse(fs.readFileSync(conf_path, 'utf8'));
     storage.get('config', function (error, data) {
         if (error) throw error;
@@ -17,13 +17,14 @@ module.exports = {
     const mainWindow = new BrowserWindow({width: 1200, height: 800});
     mainWindow.loadURL('file://' + __dirname + '/src/admin/index.html');
   },
-  get: function(){
+  get: function(callback){
     storage.get('config', function (error, data) {
         if (error) throw error;
         if (Object.keys(data).length === 0) {
-            return init_default_conf(tabs);
+          init_default_conf(tabs)
+          callback(tabs);
         }else{
-            return data;
+            callback(data);
         }
     });
   }
--- src/admin/js/render.js
+++ src/admin/js/render.js
@@ -6,17 +6,20 @@ contents → 該当するtabのitemsを
 var fs = require('fs');
 const storage = require('electron-json-storage');
 const remote = require('electron').remote;
-const preference = remote.require('./preference.json');
+const tabs = remote.require('./preference.json');
 const ClassNames = require('classnames');
 
 
 var Sidebar = React.createClass({
-  getInitialState: function(){return null},
-  handleChange: function(){return null},
   render:function(){
     const current = this.props.current;
-    const lists = preference.map(function(e,i){
-      return <li className={ClassNames({current:(current == i)})}><a>{e.name}</a></li>
+    const self = this;
+    const lists = tabs.map(function(e,i){
+      return(
+        <li key={i} data-position={i} className={ClassNames({current:(current == i)})} onClick={self.props.onClickTab}>
+          <a href="#">{e.name}</a>
+        </li>
+      )
     })
     return (
 		  	<div className="sidebar content-box" style={{display: "block"}}>
@@ -37,7 +40,7 @@ var Item = React.createClass({
         </div>
         <div className="content-box-large box-with-header">
           {this.props.item.description}<br />
-          {this.props.item.type}
+          <input name={this.props.item.name} className={this.props.item.type} type={this.props.item.type} onChange={this.props.onChangedValue}/>
         </div>
       </div>
     )
@@ -45,19 +48,54 @@ var Item = React.createClass({
 });
 
 var Body = React.createClass({
-  propTypes:{
-    current: React.PropTypes.number.isRequired
+  getInitialState: function(){
+    return {
+      current:0,
+      app_preference:{}
+    }
   },
-  getInitialState: function(){return null},
+  componentDidMount: function(){
+    const self = this;
+    storage.get('config', function (error, data) {
+      if (error) throw error;
+      self.setState({
+        app_preference: data
+      });
+    });
+	},
+  onClickTab: function(key){
+    this.setState({
+      current:key.currentTarget.getAttribute('data-position')
+    });
+  },
+  onChangedValue: function(e) {
+    const tabName = tabs[this.state.current].name;
+    var val = null;
+    switch(e.target.type){
+      case "checkbox":
+        console.log(e.target.checked);
+        val = e.target.checked;
+        break;
+
+      default:
+        val = e.target.value;
+    }
+    var current_preference = this.state.app_preference;
+    current_preference[tabName][e.target.name] = val;
+    this.setState({
+      preference: current_preference
+    });
+  },
   render: function(){
-    const items = preference[this.props.current].items.map(function(item){
-      return <Item item={item} />
+    const self = this;
+    const items = tabs[this.state.current].items.map(function(item, i){
+      return <Item item={item} key={i} onChangedValue={self.onChangedValue} />
     });
     return (
       <div className="page-content">
       	<div className="row">
     		  <div className="col-md-2">
-            <Sidebar current={this.props.current}/>
+            <Sidebar current={this.state.current} onClickTab={this.onClickTab}/>
     		  </div>
     		  <div className="col-md-10">
     		  	<div className="row">
@@ -72,5 +110,5 @@ var Body = React.createClass({
 
 
 // ReactDOM.render(<Sidebar />, document.getElementById('sidebar'));
-ReactDOM.render(<Body current="0" />, document.getElementById('body'));
+ReactDOM.render(<Body />, document.getElementById('body'));
 // ReactDOM.render(<Contents />, document.getElementById('contents'));
